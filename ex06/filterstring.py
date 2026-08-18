75248f7b8c9a782c01f22aaa825857f8ee90003c
yel-alja <yassine122alja@gmail.com>
2026-08-18 22:05:32 +0100

    add: remove lambda from the global scope

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -1,20 +1,18 @@
 from ft_filter import ft_filter
 import sys
 
-try:
-    n = int(sys.argv[2])
-    filterString = lambda s : len(s) > n
-
-except:
-    print("AssertionError: the arguments are bad")
-
 def main():
-    if len(sys.argv) != 3:
+    try:
+        n = int(sys.argv[2])
+        filterString = lambda s : len(s) > n
+        if len(sys.argv) != 3:
+            print("AssertionError: the arguments are bad")
+        else:
+            res = sys.argv[1].split()
+            res2 = list(filter(filterString , res))
+            print(res2)
+    except:
         print("AssertionError: the arguments are bad")
-    else:
-        res = sys.argv[1].split()
-        res2 = list(filter(filterString , res))
-        print (res2)
 
 if __name__ == "__main__":
     main()
