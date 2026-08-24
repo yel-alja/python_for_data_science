071ee693a849edbeef6010efded3883dc545d288
Yassine El-Alja <yel-alja@c1r9p8.1337.ma>
2026-08-24 10:38:43 +0100

    fix: number of demension in ft_load return to 3 instead of 2

--- python-1-Array/ex02/load_image.py
+++ python-1-Array/ex02/load_image.py
@@ -5,6 +5,5 @@ from PIL import Image
 def ft_load(path: str):
     image = Image.open(path)
     image = image.convert("RGB")
-
     print(f"The shape of image is: {np.array(image).shape}")
-    return( np.array(list(image.getdata())))
+    return  np.array(list(image.getdata()) , ndmin=3)
