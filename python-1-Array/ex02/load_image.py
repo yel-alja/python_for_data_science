97d5ba38bae769fe19ef670714299e25e90bbe23
Yassine El-Alja <yel-alja@c1r9p8.1337.ma>
2026-08-24 10:00:45 +0100

    add: ex02

---
+++ python-1-Array/ex02/load_image.py
@@ -0,0 +1,10 @@
+import numpy as np
+from PIL import Image
+
+
+def ft_load(path: str):
+    image = Image.open(path)
+    image = image.convert("RGB")
+
+    print(f"The shape of image is: {np.array(image).shape}")
+    return( np.array(list(image.getdata())))
