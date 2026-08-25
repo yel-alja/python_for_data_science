6200c354c0edd976036631302f9dd32e957c8520
Yassine El-Alja <yel-alja@c1r7p6.1337.ma>
2026-08-25 15:25:37 +0100

    fix: output in ex04

--- python-1-Array/ex04/rotate.py
+++ python-1-Array/ex04/rotate.py
@@ -1,15 +1,15 @@
 from PIL import Image
 from load_image import ft_load
+import matplotlib.pyplot as plt
 import os
 
 def main():
     print(ft_load("animal.jpeg"))
     with Image.open("animal.jpeg") as im:
         cuted = im.crop((im.width / 4 , im.height / 4 , 4 * im.width /  4  , 4 * im.height/ 4))
-        cuted.rotate(90).show()
-        cuted.save("res.jpeg")
-        ft_load('res.jpeg')
-        os.remove('res.jpeg')
+        cuted = cuted.rotate(90).convert("L")
+        plt.imshow(cuted, cmap="gray")
+        plt.show()
 
 
 
---
+++ python-1-Array/ex05/pimp_image.py
@@ -0,0 +1,9 @@
+def ft_invert(array) -> array:
+#your code here
+def ft_red(array) -> array:
+#your code here
+def ft_green(array) -> array:
+#your code here
+def ft_blue(array) -> array:
+#your code here
+def ft_grey(array) -> a:
