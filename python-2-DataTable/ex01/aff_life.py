2aaf75030525a78513bc72872c9d157c29d87d89
yel-alja <yassine122alja@gmail.com>
2026-08-31 16:21:04 +0100

    add: ex01

---
+++ python-2-DataTable/ex01/aff_life.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from load_csv import load
+
+def main():
+    load("life_expectancy_years.csv")
+    data  = pd.read_csv("life_expectancy_years.csv")
+    years = data.columns[1:]
+    morocco = data[data["country"] == "Morocco"].iloc[0]
+    plt.plot(years, morocco[1:])
+    plt.title("Morocco Life Expectancy Projections")
+    plt.xlabel("year")
+    plt.ylabel("Life Expectancy")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
